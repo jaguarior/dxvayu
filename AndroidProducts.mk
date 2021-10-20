bf9656dc7bf7bb92265cc95693d8f66a482d6856
Jagaddhita Jalu <88699126+jaguarezz@users.noreply.github.com>
2021-10-21 06:10:39 +0700

    vayu: BananaDroid bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/octavi_vayu.mk
+    $(LOCAL_DIR)/banana_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    octavi_vayu-eng \
-    octavi_vayu-userdebug \
-    octavi_vayu-user
+    banana_vayu-eng \
+    banana_vayu-userdebug \
+    banana_vayu-user
